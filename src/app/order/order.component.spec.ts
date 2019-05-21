8632cdf09a4f9be6d7693d939cfb92fa03c46938
eunjuhuss <eunjuhuss@hotmail.com>
2019-05-21 14:09:33 +0200

    added testing module and http module in order component spec

--- src/app/order/order.component.spec.ts
+++ src/app/order/order.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderComponent } from './order.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('OrderComponent', () => {
   let component: OrderComponent;
@@ -8,7 +10,8 @@ describe('OrderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrderComponent ]
+      declarations: [ OrderComponent ],
+      imports: [ HttpClientModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
