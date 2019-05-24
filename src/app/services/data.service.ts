f64e5673f28396fd0531403db2f33081af4246f9
eunjuhuss <eunjuhuss@hotmail.com>
2019-05-24 14:34:49 +0200

    added amount to localstorage and can push an new item

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,71 +1,145 @@
 import { Injectable } from '@angular/core';
+
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
+
 import { IProduct } from '../interfaces/iproduct';
+
 import { IData } from '../interfaces/idata';
+
 import { ICategory } from '../interfaces/icategory';
+
 import { ICart } from '../interfaces/ICart';
 
+
+
 @Injectable({
+
   providedIn: 'root'
+
 })
+
 export class DataService implements IData{
 
+
+
   filmsUrl: string = 'https://medieinstitutet-wie-products.azurewebsites.net/api/products';
+
   categoryUrl: string = 'https://medieinstitutet-wie-products.azurewebsites.net/api/categories';
+
   // productsUrl: string = 'https://medieinstitutet-wie-products.azurewebsites.net/api/products/';
+
+
 
   constructor(private httpClient: HttpClient) { }
 
 
+
+
+
  
 
+
+
 getData(): Observable<IProduct[]>{  
+
     return this.httpClient.get<IProduct[]>(this.filmsUrl); 
+
  } 
 
+
+
 getCategory(): Observable<ICategory[]>{  
+
    return this.httpClient.get<ICategory[]>(this.categoryUrl); 
+
 } 
 
+
+
 getMovie(id: number): Observable<IProduct>{
+
   return this.httpClient.get<IProduct>(this.filmsUrl + '/'+ id); 
+
 }
+
+
+
+
 
 
 
 addProductToCart(product:IProduct){
   let cart: ICart[] = this.getProductFromCart();
+  let addedFilm = false;
   if(cart != null) {
-    for(let i = 0; i< cart.length; i++){
-      cart[i].amount++;
+    for(let i = 0; i < cart.length; i++){
+      if(cart[i].product.id === product.id){
+        cart[i].amount++;
+        addedFilm = true;
+        cart[i].total += cart[i].product.price;               
+      }         
     }
-    // cart.push({ product: product, amount: 1, total: 0});
-    localStorage.setItem("ShoppingCart", JSON.stringify(cart));    
+    
+    if(addedFilm === false) {
+      cart.push({ product: product, amount: 1, total: product.price}); 
+    }
+      localStorage.setItem("ShoppingCart", JSON.stringify(cart));
+    }
+    else {    
+      localStorage.setItem("ShoppingCart", JSON.stringify([{ product: product, amount: 1, total: product.price}]));
+    }
   }
-  else {
-    localStorage.setItem("ShoppingCart", JSON.stringify([{ product: product, amount: 1, total: 0}]));
-  }
-}
+
+
+
+
+
+
+
+
+
+
 
 getProductFromCart(){  
+
   return JSON.parse(localStorage.getItem("ShoppingCart"));
+
 }
 
+
+
 removeProductFromCart(product){  
+
   let findProduct = this.getProductFromCart();  
+
   findProduct.splice(product, 1);
+
   this.addProductToCart(findProduct);
+
  }
 
 
+
+
+
 removeAllProductFromCart(){  
+
   localStorage.removeItem("ShoppingCart"); 
+
 }
+
+
+
 
 
 errorHandler(error: HttpErrorResponse){
+
   return Observable.throw(error.message || "Server Error");
+
 }
+
+
 
 }
